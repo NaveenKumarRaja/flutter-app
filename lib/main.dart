df4fb43c20d5c9af637f91a2dc973758ce726ead
NaveenKumar <rnaveenk666@gmail.com>
2021-03-01 16:49:14 +0530

    git commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:myapp/tabs/Account.dart';
+import 'package:myapp/tabs/Customers.dart';
+import 'package:myapp/tabs/items.dart';
 
 void main() => runApp(MaterialApp(
       debugShowCheckedModeBanner: false,
@@ -10,44 +13,55 @@ class MyApp extends StatefulWidget {
   _MyAppState createState() => _MyAppState();
 }
 
-class _MyAppState extends State<MyApp> {
+class _MyAppState extends State<MyApp> with SingleTickerProviderStateMixin {
+  TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 5, vsync: this);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Center(child: Text('Balaji Garments')),
-      ),
-      drawer: Drawer(
-        child: ListView(
-          padding: EdgeInsets.zero,
-          children: const <Widget>[
-            DrawerHeader(
-              decoration: BoxDecoration(
-                color: Colors.blue,
-              ),
-              child: Text(
-                'B   G ',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 100,
-                ),
-              ),
+          title: Center(child: Text('Balaji Garments')),
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.search),
+              onPressed: () {},
             ),
-            ListTile(
-              leading: Icon(Icons.person),
-              title: Text('Customers'),
-            ),
-            ListTile(
-              leading: Icon(Icons.shopping_cart_outlined),
-              title: Text('Items'),
-            ),
-            ListTile(
-              leading: Icon(Icons.account_balance),
-              title: Text('Account Details'),
+            IconButton(
+              icon: Icon(Icons.add),
+              onPressed: () {},
             ),
           ],
-        ),
+          bottom: TabBar(
+            controller: _tabController,
+            tabs: [
+              Tab(
+                icon: Icon(Icons.person),
+              ),
+              Tab(
+                icon: Icon(Icons.shopping_cart),
+              ),
+              Tab(
+                icon: Icon(Icons.account_balance),
+              ),
+            ],
+          )),
+      body: new TabBarView(
+        controller: _tabController,
+        children: <Widget>[
+          new Customers(),
+          new Items(),
+          new Accounts(),
+        ],
       ),
     );
   }
 }
+// Copyright 2019 The Flutter team. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
