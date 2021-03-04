f545517c0583accfca652e75665b3ccaa23fb7a4
NaveenKumar <rnaveenk666@gmail.com>
2021-03-04 20:58:57 +0530

    git commit

--- lib/Customers.dart
+++ lib/Customers.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:myapp/forms.dart';
 import 'package:myapp/main.dart';
 
 class Customers extends StatelessWidget {
@@ -6,103 +7,79 @@ class Customers extends StatelessWidget {
   Widget build(BuildContext context) {
     return new Scaffold(
         drawer: new DrawerCodeOnly(),
-        appBar: new AppBar(
-          title: new Text("Customers"),
-        ),
+        appBar: new AppBar(title: new Text("Customers"), actions: <Widget>[
+          FloatingActionButton(
+            onPressed: () {
+              Navigator.push(context,
+                  new MaterialPageRoute(builder: (context) => new Forms()));
+            },
+            child: Icon(Icons.add),
+          ),
+        ]),
         body: MyStatefulWidget());
   }
+}
+
+class Customer {
+  static List<Customer> customers = new List<Customer>();
+  String name;
+  String phoneNumber;
+  String address;
+
+  static Customers customer;
+
+  static List<Customer> getCustomers() {
+    /*List<Customer> customers = new List<Customer>();
+    Customer customer1 = new Customer();
+    customer1.name = "Naveen";
+    customer1.phoneNumber = "87585766";
+    customer1.address = "tau";
+    customers.add(customer1);
+    Customer customer2 = new Customer();
+    customer2.name = "Naven";
+    customer2.phoneNumber = "87585766";
+    customer2.address = "tau";
+   customers.add(customer2); */
+    return customers;
+  }
+
+  static void addCustomer(Customer customer) {
+    customers.add(customer);
+  }
+
+  void add(Customer customer) {}
 }
 
 class MyStatefulWidget extends StatelessWidget {
   MyStatefulWidget({Key key}) : super(key: key);
 
   Widget build(BuildContext context) {
-    return DataTable(
-      columns: const <DataColumn>[
-        DataColumn(
-          label: Text(
-            'Customer Name',
-            style: TextStyle(fontStyle: FontStyle.italic),
-          ),
-        ),
-        DataColumn(
-          label: Text(
-            'Phone No',
-            style: TextStyle(fontStyle: FontStyle.italic),
-          ),
-        ),
-        DataColumn(
-          label: Text(
-            'Address',
-            style: TextStyle(fontStyle: FontStyle.italic),
-          ),
-        ),
-      ],
-      rows: const <DataRow>[
-        DataRow(
-          cells: <DataCell>[
-            DataCell(Text('Sarathi')),
-            DataCell(Text('789256919')),
-            DataCell(Text('Sulur')),
-          ],
-        ),
-        DataRow(
-          cells: <DataCell>[
-            DataCell(Text('Johny')),
-            DataCell(Text('9548894443')),
-            DataCell(Text('Perundurai')),
-          ],
-        ),
-        DataRow(
-          cells: <DataCell>[
-            DataCell(Text('William')),
-            DataCell(Text('9834563427')),
-            DataCell(Text('Tirupur')),
-          ],
-        ),
-        DataRow(
-          cells: <DataCell>[
-            DataCell(Text('Sarathi')),
-            DataCell(Text('789256919')),
-            DataCell(Text('Sulur')),
-          ],
-        ),
-        DataRow(
-          cells: <DataCell>[
-            DataCell(Text('Jaga')),
-            DataCell(Text('95483494443')),
-            DataCell(Text('Perundurai')),
-          ],
-        ),
-        DataRow(
-          cells: <DataCell>[
-            DataCell(Text('Alagar')),
-            DataCell(Text('9834453427')),
-            DataCell(Text('Tiruvanamalai')),
-          ],
-        ),
-        DataRow(
-          cells: <DataCell>[
-            DataCell(Text('Saraswathi')),
-            DataCell(Text('8789256919')),
-            DataCell(Text('Sundharapuam')),
-          ],
-        ),
-        DataRow(
-          cells: <DataCell>[
-            DataCell(Text('Joe')),
-            DataCell(Text('9548894433')),
-            DataCell(Text('Perumanullr')),
-          ],
-        ),
-        DataRow(
-          cells: <DataCell>[
-            DataCell(Text('Barathan')),
-            DataCell(Text('9834565627')),
-            DataCell(Text('Tirupathi')),
-          ],
-        ),
-      ],
-    );
+    return ListView(children: <Widget>[
+      Center(
+          child: Text(
+        'Customer Details',
+        style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+      )),
+      DataTable(
+        columns: [
+          DataColumn(
+              label: Text('Customer Name',
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold))),
+          DataColumn(
+              label: Text('Phone No',
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold))),
+          DataColumn(
+              label: Text('Address',
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold))),
+        ],
+        rows: Customer.getCustomers()
+            .map((customer) => DataRow(cells: [
+                  DataCell(Text(customer.name)),
+                  DataCell(Text(customer.phoneNumber)),
+                  DataCell(Text(customer.address)),
+                ]))
+            .toList(),
+      ),
+    ]);
   }
 }
